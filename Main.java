0b143c860a45e7f207f763350ca89c1f1f241459
unknown <sivovol.dmitrii1988@gmail.com>
2021-03-03 21:02:35 +0200

    adding home work 4

---
+++ Main.java
@@ -0,0 +1,21 @@
+package dz4;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число a");
+        int a = scanner.nextInt();
+        System.out.println("Введите число b");
+        int b = scanner.nextInt();
+       int c;
+        if ( a <= b)
+        { System.out.println( c = a + b);}
+else
+        {c = a - b;
+        System.out.println( c = a-b );}
+    }
+
+}
